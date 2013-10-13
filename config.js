6db2568f0ea15eec4471b168dcfb6914e55048a1
Eddie Flores <eddflrs@gmail.com>
2013-10-13 02:10:33 -0400

    Adds metroly to openshift config

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config.dev = {
 config.prod = {
   PORT: process.env.OPENSHIFT_NODEJS_PORT,
   IP: process.env.OPENSHIFT_NODEJS_IP,
-  MONGO_URL: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/'
+  MONGO_URL: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/metroly'
 };
 
 module.exports = (state === 'prod') ? config.prod : config.dev;
